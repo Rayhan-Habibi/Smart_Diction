e2accd6417f9a7df961cb484998f74ff2243bf7b
Rayhan-Habibi <nahyaribibah2017@gmail.com>
2023-07-04 17:20:21 +0700

    Fixed search bar

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -23,7 +23,7 @@ export default function SearchBar() {
   return (
     <div className='flex items-center lg:p-0 p-5 justify-center flex-col bg-white backdrop-blur-sm rounded-xl z-0 mx-3'>
         <form className="flex items-center">
-            <input onChange={handleChange} value={input} type="text" className='focus:outline-none border-2 border-black h-16 rounded-xl lg:w-96 text-2xl pl-14 bg-white/50 backdrop-blur-sm' placeholder='Browse' />
+            <input onChange={handleChange} value={input} type="text" className='focus:outline-none border-2 border-black w-80 h-16 rounded-xl lg:w-96 text-2xl pl-14 bg-white/50 backdrop-blur-sm' placeholder='Browse' />
             <Icon icon="system-uicons:search" className='mr-5 ml-2 absolute' width="40" height="40"/>
             <button onClick={handleInput} className='text-2xl border-black ml-3 lg:static hidden border-2 p-3.5 active:scale-95 duration-100 hover:text-violet-800 rounded-xl bg-white/50 backdrop-blur-sm'>Submit</button>
         </form> 
